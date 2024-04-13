b5ddad3bc63db5ad29d31c230efeb52b70b1d195
Artur Matos Malcher de Sousa <arturmatosmalcherdesousa@gmail.com>
2024-04-13 18:03:38 -0300

    Cliente Atualizado; get_registros; get_clientes

--- infra/data/gerenciamento.py
+++ infra/data/gerenciamento.py
@@ -1,7 +1,9 @@
-from bd_classes import session, Cliente, Funcionario, Sessao
+from sqlalchemy import func
+from bd_classes import session, Cliente, Funcionario, Sessao, Registro
 from datetime import datetime
+from dateutil import relativedelta
 from passlib.hash import argon2
-
+from typing import List
 
 # GESTÃO DE USUARIOS
 
@@ -9,6 +11,8 @@ def cadastro_cliente(nome:str,
                      cpf:str,
                      rg:str,
                      filiacao:str,
+                     estado:str,
+                     municipio:str,
                      endereco:str,
                      data_nascimento:str,
                      telefone:str=None,
@@ -20,9 +24,11 @@ def cadastro_cliente(nome:str,
         nome (str): Nome completo do cliente.
         cpf (str): Número de CPF do cliente.
         rg (str): Número de RG do cliente.
-        filiacao (str): Nome do pai ou mãe do cliente
+        filiacao (str): Nome do pai ou mãe do cliente.
+        estado (str): Estado em que o cliente está localizado.
+        municipio (str): Cidade em que o cliente está localizado.
         endereco (str): Endereço do cliente.
-        data_nascimento (str, dd-mm-aaaa): Data de nascimento do cliente.
+        data_nascimento (str): Data de nascimento do cliente (formato dd-mm-aaaa).
         telefone (str, opcional): Número de telefone do cliente.
         email (str, opcional): Endereço de e-mail do cliente.
 
@@ -35,6 +41,8 @@ def cadastro_cliente(nome:str,
                     cpf=cpf,
                     rg=rg,
                     filiacao=filiacao,
+                    estado=estado,
+                    municipio=municipio,
                     endereco=endereco,
                     data_nascimento=datetime.strptime(data_nascimento, "%d-%m-%Y"),
                     telefone=telefone,
@@ -48,7 +56,7 @@ def cadastro_cliente(nome:str,
         return novo_cliente
     except Exception as e:
         print(f"Erro ao cadastrar cliente: {e}")
-
+    
 
 def cadastro_funcionario(nome:str, cpf:str, telefone:str, email:str, senha:str) -> Funcionario:
     """Adiciona um Funcionario ao Banco de Dados.
@@ -125,3 +133,94 @@ def encerrar_sessao(sessao: Sessao):
         session.commit()
     except Exception as e:
         raise Exception(f"Erro ao registrar fim de sessão: {e}")
+    
+
+# CONSULTAS
+
+def get_clientes(id:int=None,
+                nome:str=None,
+                cpf:str=None,
+                ) -> List[Cliente]:
+    """Retorna uma lista de Clientes com base nos argumentos.
+    OBS: Se não houver argumentos, retorna todos os clientes.
+
+    Args:
+        id (int, opcional): filtrar por id único de um cliente.
+        nome (str, opcional): filtrar por nome completo do cliente.
+        cpf (str, opcional): filtrar por número de CPF do cliente.
+
+    Returns:
+        List[Cliente]: Uma lista de Clientes que coincidem com os argumentos.
+    """
+    kwargs = {}
+    if id:
+        kwargs["id"] = id
+        
+    if nome:
+        kwargs["nome"] = nome
+        
+    if cpf:
+        kwargs["cpf"] = cpf
+    
+    try:
+        clientes = session.query(Cliente).filter_by(**kwargs)
+        return clientes.all() 
+    except Exception as e:
+        print(f"Erro ao procurar Cliente: {e}")
+
+
+def get_registros(id: int = None,
+                  fk_sessao: int = None,
+                  dia: str = None,
+                  mes: str = None,
+                  ano: str = None,
+                  ) -> List[Registro]:
+    """Retorna uma lista de Registros com base nos argumentos.
+    OBS: Se não houver argumentos, retorna todos os registros.
+
+    Args:
+        id (int, opcional): filtrar por id único do registro.
+        fk_sessao (int, opcional): filtrar por id de uma sessão específica.
+        dia (str, opcional): filtrar por dia específico (formato dd-mm-aaaa).
+        mes (str, opcional): filtrar por mês específico (formato mm-aaaa).
+        ano (str, opcional): filtrar por ano específico (formato aaaa).
+
+    Returns:
+        List[Registro]: Lista de registros filtrados.
+    """
+
+    kwargs = {}
+    if id:
+        kwargs["id"] = id
+
+    if fk_sessao:
+        kwargs["fk_sessao"] = fk_sessao
+
+    # Condições de filtro (filtragem vazia se nenhum argumento for válido)
+    filtros = []
+    
+    if dia and datetime.strptime(dia, "%d-%m-%Y").date() is not None:
+        filtros.append(func.date(Registro.horario) == datetime.strptime(dia, "%d-%m-%Y").date())
+
+    if mes:
+        # Manter a filtragem por mês e ano específicos (se for o caso)
+        data_inicio = datetime.strptime("01-"+mes, "%d-%m-%Y").date()
+        data_fim = data_inicio + relativedelta.relativedelta(months=+1, day=1) - relativedelta.relativedelta(days=1)
+        filtros.append(func.date(Registro.horario) >= data_inicio)
+        filtros.append(func.date(Registro.horario) < data_fim)
+        
+    if ano:
+        data_inicio = datetime.strptime("01-01-"+ano, "%d-%m-%Y").date()
+        data_fim = datetime.strptime("31-12-"+ano, "%d-%m-%Y").date()
+        filtros.append(func.date(Registro.horario) >= data_inicio)
+        filtros.append(func.date(Registro.horario) <= data_fim)
+
+    # Construir a consulta
+    try:
+        registros = session.query(Registro).filter_by(**kwargs)
+        if filtros:
+            registros = registros.filter(*filtros) 
+        return registros.all()
+    except Exception as e:
+        print(f"Erro ao procurar Registro: {e}")
+        
